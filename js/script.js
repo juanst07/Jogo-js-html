f1929c13a6b2b17558f9a1149d2c0b371936874d
juanst07 <0001068289@senaimgaluno.com.br>
2025-06-02 14:39:29 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const mario = document.querySelector('.mario');
-const gameOverImg = document.querySelector('.game-over'); // Adicione isso
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
- mario.classList.add('jump');
+    mario.classList.add('jump');
 
     setTimeout(() => {
         mario.classList.remove('jump');
@@ -11,33 +11,27 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
-    console.log('loop');
+  console.log('loop')
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(marioPosition);
-    
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
     pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`;
 
     mario.style.animation = 'none';
-    mario.style.left = `${marioPosition}px`;
+    mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = 'img/game-over.png';
+    mario.src = './imagens/game-over.png';
     mario.style.width = '75px';
     mario.style.marginLeft = '50px';
 
-    if (gameOverImg) {
-        gameOverImg.style.display = 'block'; // Mostra a imagem de game over
-    }
-
     clearInterval(loop);
 
-}
+  }
 
-},10);
+}, 10)
 
 document.addEventListener('keydown', jump);
